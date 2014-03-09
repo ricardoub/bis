81d3008cfe97f57f9dc23b6af3591b045faafee0
Ricardo Bomfim <ricardoub@gmail.com>
2014-03-09 19:01:49 -0300

    Inclusão do capitulo 5.4

--- src/br/com/casadocodigo/bis/game/objects/Meteor.java
+++ src/br/com/casadocodigo/bis/game/objects/Meteor.java
@@ -1,9 +1,31 @@
 package br.com.casadocodigo.bis.game.objects;
 
-public class Meteor {
+import java.util.Random;
 
-	public Meteor(String meteor) {
-		// TODO Auto-generated constructor stub
+import org.cocos2d.nodes.CCSprite;
+import org.cocos2d.types.CGPoint;
+
+import static br.com.casadocodigo.bis.config.DeviceSettings.screenHeight;
+import static br.com.casadocodigo.bis.config.DeviceSettings.screenWidth;
+import static br.com.casadocodigo.bis.config.DeviceSettings.screenResolution;
+
+
+public class Meteor extends CCSprite {
+	private float x, y;
+
+	public Meteor(String image) {
+		super(image);
+		x = new Random().nextInt(Math.round(screenWidth()));
+		y = screenHeight();
+	}
+	
+	public void start() {
+		this.schedule("update");
+	}
+	
+	public void update(float dt) {
+		y -= 1;
+		this.setPosition(screenResolution(CGPoint.ccp(x, y)));
 	}
 
 }
